5074351102e6463cffe6caeb25571bf935af39c2
szymbaramichal <szymbaramichal@gmail.com>
2024-12-29 20:13:36 +0100

    Initial commit

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartyRoleInfoApp.Controllers;
+
+public class RolesController : Controller
+{
+    public IActionResult Skipper()
+    {
+        return View();
+    }
+
+    public IActionResult Wishlister()
+    {
+        return View();
+    }
+    
+    public IActionResult FreeBather()
+    {
+        return View();
+    }
+
+    public IActionResult Instaxer()
+    {
+        return View();
+    }
+
+    public IActionResult BeerpongFriend()
+    {
+        return View();
+    }
+
+    public IActionResult FreeBeerer()
+    {
+        return View();
+    }
+
+    public IActionResult Playlister()
+    {
+        return View();
+    }
+
+    public IActionResult Cheerser()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PartyRoleInfoApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
